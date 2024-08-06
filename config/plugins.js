5b410cd8e5d5c13f19b70165a6b97f2f4a791f59
Indrajit <indrajitghosh.dev@gmail.com>
2024-08-07 01:49:29 +0530

    smtp added

--- config/plugins.js
+++ config/plugins.js
@@ -27,4 +27,16 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  email: {
+    config: {
+      provider: 'strapi-provider-email-resend',
+      providerOptions: {
+        apiKey: env('RESEND_API_KEY'),
+      },
+      settings: {
+        defaultFrom: 'support@multikit.shop',
+        defaultReplyTo: 'indrajitghosh.dev@gmail.com',
+      },
+    }
+  },   
 });
